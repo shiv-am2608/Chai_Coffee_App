693650c52a8247a501456a976c60388ab43987e1
shiv-am2608 <1805428@kiit.ac.in>
2021-01-24 15:09:51 +0530

    Initial Commit

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -13,6 +13,7 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    int quantity=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +25,19 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int nc=2;
-        display(nc);
-        displayPrice(nc*5);
+        String priceMessage="Total Price: ₹"+(quantity*5)+"\nThank You!!";
+        displayMessage(priceMessage);
+        //displayPrice(quantity*5);
+    }
+    public void increment(View view)
+    {
+        quantity++;
+        display(quantity);
+    }
+    public void decrement(View view)
+    {
+        quantity--;
+        display(quantity);
     }
 
     /**
@@ -42,5 +53,12 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
